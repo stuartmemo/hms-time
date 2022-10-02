778122f288c2516451b00abb9f29b54eb15e9460
Stuart Memo <stuartmemo@gmail.com>
2022-10-02 12:53:50 +0100

    Removed gulp

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var mocha = require('gulp-mocha');
-
-gulp.task('default', function () {
-    return gulp.src('tests.js')
-        .pipe(mocha({reporter: 'nyan'}));
-});
